85f02104e24a98f5836b59fc4618d499e19df603
akino <akikoba2001@gmail.com>
2023-05-21 19:18:40 +0900

    sample app

---
+++ app/src/data/shops.ts
@@ -0,0 +1,16 @@
+export const GeoJson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [139.71717416931216, 35.70845418406614]
+            },
+            properties: {
+                name: '面珍',
+                star: 5,
+            }
+        },
+    ]
+}
