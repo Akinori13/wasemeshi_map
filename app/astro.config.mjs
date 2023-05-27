51a33daf1ce0d5f66f78dc7f9f4f93e92ffd93ad
akino <akikoba2001@gmail.com>
2023-05-27 12:11:25 +0900

    リリース0527

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,8 +4,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	// site: "https://Akinori13.github.io",
-	// base: "/wasemeshi_map",
 	output: "server",
 	adapter: vercel(),
 });
